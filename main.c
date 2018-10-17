d1956b827143a1f7a3ffd471736419856398a1b0
杨生曼 <43878365+Yangshengman@users.noreply.github.com>
2018-10-17 21:43:43 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,64 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-float sum;
-void Add(float op1,float op2)
+int RSA(int baseNum,int key ,int msg)
 {
-    sum = (float)op1 + op2;
-    printf("%.2f\n",sum);
+    int RSAmsg = 1;
+    key = key =1;
+    while (key!=1)
+    {
+        RSAmsg=RSAmsg*msg;
+          RSAmsg=RSAmsg%baseNum;
+          key--;
 
+    }
+    return RSAmsg;
 
 }
-   void Sub(float op1,float op2)
-{
-    sum = op1 - op2;
-     printf("%.2f\n",sum);
-
-}
-    void Mult(float op1,float op2)
-{
-    sum = op1 * op2;
-     printf("%.2f\n",sum);
-
-}
-  void Div(float op1,float op2)
-{
-    if (op2 == 0)
-     printf("被除数不能为0!");
-     else{
-        sum =op1/op2;
-        printf("%.2f\n",sum);
-
-     }
-
-}
-
-
 int main()
 {
-    float op1,op2;
-    char ch;
-    while (1)
-    {
-        scanf("%f%c%f",&ch,&op2);
-        swich(ch)
-        {
-        case '+':
-            Add (op1,op2);
-            break;
-            case '-':
-             Sum (op1,op2);
-            break;
-            case '*':
-             Mult (op1,op2);
-            break;
-            case '/':
-             Div (op1,op2);
-            break;
-            default:
-                break;
+   int p,q,baseNum,Euler,r;
+   int keyE,keyD,m_mag,c_msg;
+   printf("请输入p,q:");
+   scanf("%d%d",&p,&q);
+   baseNum = p*q;
+   Euler = (p-1)*(q-1);
+   printf("请输入公钥(与%d互质）：",Euler);
+   scanf("%d",&keyE);
+   while(keyE<1 || keyE>keyE);
+   {
+     printf("输入错误!\n请重新输入：");
+        ccanf("%d",&keyE);
 
-        }
-    }
+
+   }
+   keyD = 1;
+   while(((keyE*keyD) % Euler) != 1)
+    keyD++;
+   printf("私密：%d\n",keyD);
+   printf("1.加密\n");
+     printf("2.解密\n");
+       printf("3.退出\n");
+       while(1)
+       {
+           printf("请选择要执行的操作:");
+           scanf("%d",&r);
+           switch(r)
+           {
+           case 1:
+            printf("请输入要加密的数据:");
+            scanf ("%d",&m_msg);
+            c_msg = RSA (baseNum,keyE,m_msg);
+            printf("加密后的数据为:%d\n",c_msg);
+            break;
+            case 2:
+            printf("请输入要解密的数据:");
+            scanf("%d",&m_msg);
+            c_msg = RSA(baseNum,keyD,c_msg);
+            printf("解密后的数据为:%d\n",m_msg);
+            break;
+            case3:
+                exit(0);
+                break;
+            default:
+                printf("选择有误\n");
+                break;
+           }
+       }
 
     return 0;
 }
